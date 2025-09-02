60f1d4f9585e8a59e4d71f83b8172405a94585a9
VamshiRCV10 <rachapallychandravamshi@gmail.com>
2025-09-02 18:55:41 +0530

    Updated Root

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:5000'
+    : process.env.REACT_APP_API_URL;
+
+export default axios;
